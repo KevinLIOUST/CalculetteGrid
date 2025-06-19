211b39720cb6f82cd4d9b074f80855ebd861edb4
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-19 15:45:23 +0200

    Opérations de bases avec des entiers terminé ! :)

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// Fonction qui permet d'afficher le nombre en question.
+function affichageNombre(nombre) {
+  document.getElementById("affichage").innerText += nombre;
+}
+
+// Fonction qui va effacer le texte des chiffres
+function effacerTexte() {
+    document.getElementById("affichage").innerText = '';
+}
+
+// Fonction qui va afficher les operateurs
+function affichageOperateur(operateur) {
+  switch (operateur) {
+    case '/':
+      document.getElementById("affichage").innerText += "\u00F7";
+      break;
+    case '*':
+      document.getElementById("affichage").innerText += "\u00D7";
+      break;
+    case '+':
+      document.getElementById("affichage").innerText += "+";
+      break;
+    case '-':
+      document.getElementById("affichage").innerText += "-";
+      break;
+  }
+}
+
+// Fonction pour calculer le résultat
+function calculerResultat() {
+
+    // Ingrediants
+    let resultat = 0;
+    let operateur = '';
+    let nb1 = 0;
+    let nb2 = 0;
+
+    // On récupère le texte de la calculette pour travailler dessus.
+    let texte = document.getElementById("affichage").getHTML();
+    console.log(texte);
+
+    // On parcours la chaîne de caractères pour trouver l'operateur.
+    for (let i=0; i < texte.length; i++) {
+        if (isNaN(texte.charAt(i))) {
+            operateur = texte.charAt(i);
+        }
+    }
+
+    let tabNombres = texte.split(operateur);
+
+    nb1 = parseInt(tabNombres[0]);
+    nb2 = parseInt(tabNombres[1]);
+
+    if (operateur == '+') {
+        resultat = nb1 + nb2;
+        document.getElementById("affichage").innerText = resultat;
+        return resultat;
+    } else if (operateur == '-') {
+        resultat = nb1 - nb2;
+        document.getElementById("affichage").innerText = resultat;
+        return resultat;
+    } else if (operateur == '\u00F7') {
+        resultat = nb1 / nb2;
+        document.getElementById("affichage").innerText = resultat;
+        return resultat;
+    } else if (operateur == '\u00D7') {
+        resultat = nb1 * nb2;
+        document.getElementById("affichage").innerText = nb1 * nb2;
+        return resultat;
+    }
+}
