fbaf2eb05c990c5cfb10320bf3010fd37685c2c6
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-20 09:58:11 +0200

    Fonction pour supprimer un caractère Terminée ! :)

--- js/script.js
+++ js/script.js
@@ -5,80 +5,100 @@ function allumerCalculette() {
 
 // Fonction qui permet d'éteindre la calculette
 function eteindreCalculette() {
-  document.getElementById("affichage").innerText = '';
+  document.getElementById("affichage").innerText = "";
 }
 
 // Fonction qui permet d'afficher le nombre en question.
 function affichageNombre(nombre) {
-  if (document.getElementById("affichage").innerText.charAt(0) == '0') {
-    document.getElementById("affichage").innerText = nombre;
+  if (document.getElementById("affichage").innerText == "") {
+    console.log("La calculette est éteinte ! :)");
   } else {
-    document.getElementById("affichage").innerText += nombre;
+    if (document.getElementById("affichage").innerText.charAt(0) == "0") {
+      document.getElementById("affichage").innerText = nombre;
+    } else {
+      document.getElementById("affichage").innerText += nombre;
+    }
   }
 }
 
 // Fonction qui va effacer le texte des chiffres
 function effacerTexte() {
+  document.getElementById("affichage").innerText = 0;
+}
+
+// Fonction pour le bouton C qui permet d'effacer un caractère
+function effacerCaractere() {
+  let texte = document.getElementById("affichage").getHTML();
+  if (texte.length == 1) {
     document.getElementById("affichage").innerText = 0;
+  } else {
+    let tabCaracteres = texte.split("");
+    tabCaracteres.pop();
+    texte = tabCaracteres.join("");
+    document.getElementById("affichage").innerText = texte;
+  }
 }
 
 // Fonction qui va afficher les operateurs
 function affichageOperateur(operateur) {
-  switch (operateur) {
-    case '/':
-      document.getElementById("affichage").innerText += "\u00F7";
-      break;
-    case '*':
-      document.getElementById("affichage").innerText += "\u00D7";
-      break;
-    case '+':
-      document.getElementById("affichage").innerText += "+";
-      break;
-    case '-':
-      document.getElementById("affichage").innerText += "-";
-      break;
+  if (document.getElementById("affichage").innerText == "") {
+    console.log("La calculette est éteinte ! :)");
+  } else {
+    switch (operateur) {
+      case "/":
+        document.getElementById("affichage").innerText += "\u00F7";
+        break;
+      case "*":
+        document.getElementById("affichage").innerText += "\u00D7";
+        break;
+      case "+":
+        document.getElementById("affichage").innerText += "+";
+        break;
+      case "-":
+        document.getElementById("affichage").innerText += "-";
+        break;
+    }
   }
 }
 
 // Fonction pour calculer le résultat
 function calculerResultat() {
+  // Ingrediants
+  let resultat = 0;
+  let operateur = "";
+  let nb1 = 0;
+  let nb2 = 0;
 
-    // Ingrediants
-    let resultat = 0;
-    let operateur = '';
-    let nb1 = 0;
-    let nb2 = 0;
+  // On récupère le texte de la calculette pour travailler dessus.
+  let texte = document.getElementById("affichage").getHTML();
 
-    // On récupère le texte de la calculette pour travailler dessus.
-    let texte = document.getElementById("affichage").getHTML();
-
-    // On parcours la chaîne de caractères pour trouver l'operateur.
-    for (let i=0; i < texte.length; i++) {
-        if (isNaN(texte.charAt(i))) {
-            operateur = texte.charAt(i);
-        }
+  // On parcours la chaîne de caractères pour trouver l'operateur.
+  for (let i = 0; i < texte.length; i++) {
+    if (isNaN(texte.charAt(i))) {
+      operateur = texte.charAt(i);
     }
+  }
 
-    let tabNombres = texte.split(operateur);
+  let tabNombres = texte.split(operateur);
 
-    nb1 = parseInt(tabNombres[0]);
-    nb2 = parseInt(tabNombres[1]);
+  nb1 = parseInt(tabNombres[0]);
+  nb2 = parseInt(tabNombres[1]);
 
-    if (operateur == '+') {
-        resultat = nb1 + nb2;
-        document.getElementById("affichage").innerText = resultat;
-        return resultat;
-    } else if (operateur == '-') {
-        resultat = nb1 - nb2;
-        document.getElementById("affichage").innerText = resultat;
-        return resultat;
-    } else if (operateur == '\u00F7') {
-        resultat = nb1 / nb2;
-        document.getElementById("affichage").innerText = resultat;
-        return resultat;
-    } else if (operateur == '\u00D7') {
-        resultat = nb1 * nb2;
-        document.getElementById("affichage").innerText = resultat;
-        return resultat;
-    }
+  if (operateur == "+") {
+    resultat = nb1 + nb2;
+    document.getElementById("affichage").innerText = resultat;
+    return resultat;
+  } else if (operateur == "-") {
+    resultat = nb1 - nb2;
+    document.getElementById("affichage").innerText = resultat;
+    return resultat;
+  } else if (operateur == "\u00F7") {
+    resultat = nb1 / nb2;
+    document.getElementById("affichage").innerText = resultat;
+    return resultat;
+  } else if (operateur == "\u00D7") {
+    resultat = nb1 * nb2;
+    document.getElementById("affichage").innerText = resultat;
+    return resultat;
+  }
 }
